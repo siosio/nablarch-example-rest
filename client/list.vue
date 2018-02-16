eb6286c1cf5bd43bd31c60b1233c0e92e2b76223
siosio <siosio12@gmail.com>
2018-02-16 11:21:48 +0900

    簡単な一覧も表示できるように

--- client/list.vue
+++ client/list.vue
@@ -5,17 +5,56 @@
       <p>{{info_message}}</p>
     </div>
 
+    <table class="ui striped table">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>プロジェクト名</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="project in projects">
+        <td>{{project.projectId}}</td>
+        <td>{{project.projectName}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
-
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
-    name: "list",
-    props: {
+    name   : "list",
+    props  : {
       info_message: String
+    },
+    data() {
+      return {
+        projects: []
+      }
+    },
+    methods: {
+      find: function() {
+        const vm = this;
+        axios.get('http://localhost:9080/projects')
+            .then(function(response) {
+              vm.projects = response.data;
+              console.log(vm.projects);
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+      }
+    },
+    created() {
+      this.find();
+    },
+    watch  : {
+      '$route': 'find'
     }
-  }
+  };
 </script>
 
 <style scoped>
